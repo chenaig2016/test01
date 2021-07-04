c47a419b05c28d4c5969a14d2fd288e93fc40b28
chenaig2016 <863953373@qq.com>
2021-07-04 20:36:35 +0800

    3次

--- gittest01/src/main/java/com/bjpowernoe/App.java
+++ gittest01/src/main/java/com/bjpowernoe/App.java
@@ -6,8 +6,9 @@ package com.bjpowernoe;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
+        int j=4;
+        System.out.println(j);
         System.out.println( "Hello World!" );
         for(int i=3;i<6;i++){
             System.out.println(i);
