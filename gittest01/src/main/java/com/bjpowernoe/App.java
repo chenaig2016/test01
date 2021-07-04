18d63bcd5700535f91e88f9bdc5bdb6bba9160b1
chenaig2016 <863953373@qq.com>
2021-07-04 20:32:12 +0800

    2个

---
+++ gittest01/src/main/java/com/bjpowernoe/App.java
@@ -0,0 +1,16 @@
+package com.bjpowernoe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        for(int i=3;i<6;i++){
+            System.out.println(i);
+        }
+    }
+}
